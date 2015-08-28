0db921dfaa535e38dcc4222581cdb09f1acedfc0
Brian McAllister <brian@brianmcallister.com>
2015-08-28 14:02:57 -0400

    Scrub missing attributes.

--- main.js
+++ main.js
@@ -1,14 +1,79 @@
-var Thingy = require('./test');
+let Backbone = require('backbone');
+let _ = require('underscore');
 
-class Test {
-  test() {
-    console.log('test');
+let data = {one: 1, two: 2, three: 3}
+
+// Overwrite Backbone.ajax to respond with some json.
+Backbone.ajax = (args) =>
+  args.success.call(this, data);
+
+let _fetch = Backbone.Model.prototype.fetch;
+let _parse = Backbone.Model.prototype.parse;
+let opts = {};
+
+_.extend(Backbone.Model.prototype, {
+  fetch (options = {}) {
+    opts = options;
+    return _fetch.apply(this, arguments);
+  },
+
+  parse (res) {
+    if (opts.scrubMissingAttributes) {
+      for (let key in this.attributes) {
+        if (res[key] === undefined) {
+          delete res[key];
+          delete this.attributes[key];
+        }
+      }
+    }
+
+    return _parse.call(this, res);
+  }
+});
+
+// Model class.
+class Model extends Backbone.Model {
+  constructor() {
+    super();
+    this.url = 'fake';
   }
 }
 
-var test = new Test();
-test.test();
+let model = new Model();
 
-var thingy = new Thingy();
-console.log(thingy);
-console.log(thingy.getList());
+let fetch = (event) => {
+  let scrub = false;
+
+  if (event) {
+    model.set({'key': 'value'});
+    scrub = event.currentTarget.classList.contains('scrub');
+  }
+
+  model.fetch({scrubMissingAttributes: scrub});
+}
+
+let render = () => {
+  let attrs = model.toJSON();
+  let html = '';
+  let el = document.querySelector('.results-list');
+
+  // Render each item.
+  for (let key in attrs) {
+    let value = attrs[key];
+    html += `${JSON.stringify(key)}: ${JSON.stringify(value)}<br>`;
+  }
+
+  el.innerHTML = html;
+}
+
+let clear = () => {
+  model.clear();
+  render();
+}
+
+document.querySelector('.scrub').addEventListener('click', fetch);
+document.querySelector('.no-scrub').addEventListener('click', fetch);
+document.querySelector('.clear').addEventListener('click', clear);
+model.on('sync', render);
+
+fetch();
